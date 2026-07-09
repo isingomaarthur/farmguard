544aa4bd6ed3b24306505fc910f3951431920f80
isingomaarthur30-design <isingomaarthur30@gmail.com>
2026-07-09 08:28:29 +0300

    backend moved out of frontend folder

--- backend/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDashboard } from '../controllers/dashboardController.js';
-import { authenticateToken } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/', authenticateToken, getDashboard);
-
-export default router;
